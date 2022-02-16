d5e507f2558e4f73167f58a4f4ebe34e01142064
Gamspi <niko.tatt007@gmail.com>
2022-02-16 12:24:35 +0700

    Подготовка к проекту, удаление всего лишнего

---
+++ src/Core/Core.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from 'react';
+import './Core.scss';
+
+const Core: FC = () => {
+    return (
+        <div className="App">
+            Работает!!
+        </div>
+    );
+}
+export default Core;
